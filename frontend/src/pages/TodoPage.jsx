b263b5b91519fa221fc26af68948d377440dc014
Krishna Chaitanya <vanneldaskrishnachaitanya.com>
2026-04-13 12:24:55 +0530

    fix: resolve eslint exhaustive-deps warning in TodoPage preventing Vercel build

--- frontend/src/pages/TodoPage.jsx
+++ frontend/src/pages/TodoPage.jsx
@@ -20,6 +20,7 @@ export default function TodoPage() {
       return;
     }
     fetchTodos();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [user]);
 
   const fetchTodos = async () => {
